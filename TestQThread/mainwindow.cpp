e45d20ccaa0140d83f7205d64ed0891f65403d16
chenbei97 <chenbei_electric@163.com>
2023-06-30 11:37:59 +0800

    对不使用线程、重载线程、移动线程的3种方式应用

---
+++ TestQThread/mainwindow.cpp
@@ -0,0 +1,68 @@
+﻿#include "mainwindow.h"
+//#define MAXCOUNT 1000000000
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+{
+    btn1 = new QPushButton(tr("没有线程"));
+    btn2 = new QPushButton(tr("重载线程"));
+    btn3 = new QPushButton(tr("移动线程"));
+
+    connect(btn1,&QPushButton::clicked,[this] { // 主线程会卡死
+        QRandomGenerator RG(QTime::currentTime().msec());
+        long long val = 0;
+        QTime s;
+        s.start();
+        for (long long i = 0; i < MAXCOUNT; ++i)
+        {
+            val += RG.bounded(-50,100);
+            //qDebug()<<val;
+        }
+        auto t = s.elapsed() / 1000.0;
+        statusBar()->showMessage(QString("耗时: %1 s  结果: %2\n").arg(t).arg(val));
+    });
+
+    // 重载run的线程
+    // button-clicked=>mThread.start()=>run()=>ready=>showmessage
+    connect(&mThread, &MyThread::ready, this,[this](const QString&res){statusBar()->showMessage(res);});
+    //connect(&mThread, &MyThread::finished, &mThread, &QObject::deleteLater); // 不能删除否则再次点击异常
+    connect(btn2,&QPushButton::clicked,this,[this]{mThread.start();});
+
+    // 移动线程的做法
+    // mController::start(emit start)=>Calculator::doRandom=>emit ready => mController::handle
+    mController = new Controller(statusBar());
+    connect(btn3,&QPushButton::clicked,[this] {
+            mController->start(-100,100);
+    });
+
+    QHBoxLayout * lay = new QHBoxLayout;
+    lay->addStretch();
+    lay->addWidget(btn1);
+    lay->addWidget(btn2);
+    lay->addWidget(btn3);
+    lay->addStretch();
+    QGroupBox * box = new QGroupBox;
+    box->setLayout(lay);
+    setCentralWidget(box);
+    resize(800,600);
+    setFont(QFont("Times New Roman",12));
+}
+
+MainWindow::~MainWindow()
+{
+}
+
+//void MainWindow::onbtn1(){
+
+//    for (long long i = 0; i < MAXCOUNT; ++i)
+//    {
+//        statusBar()->showMessage(QString::number(RG.bounded(0,100)));
+//    }
+//}
+//void MainWindow::onbtn2(){
+
+//}
+//void MainWindow::onbtn3(){
+
+//}
+
---
+++ TestQThread/mainwindow.h
@@ -0,0 +1,104 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QPushButton>
+#include <qboxlayout.h>
+#include <QDebug>
+#include <QRandomGenerator>
+#include <QStatusBar>
+#include <QGroupBox>
+#include <QThread>
+#include <QTime>
+
+#define MAXCOUNT 100000000
+
+class MyThread : public QThread{
+    Q_OBJECT
+private:
+    void run() override
+    {
+        QRandomGenerator RG(QTime::currentTime().msec());
+        long long result = 0;
+        QTime s;
+        s.start();
+        for (long long i = 0; i < MAXCOUNT; ++i)
+        {
+            //result += RG.bounded(low,high); // 无法传递外部参数
+            result += RG.bounded(-50,88);
+        }
+        auto t = s.elapsed() / 1000.0;
+        emit ready(QString("耗时: %1 s  结果: %2\n").arg(t).arg(result));
+    }
+signals:
+    void ready(const QString&res);
+};
+
+class  Calculator : public QObject
+{
+        Q_OBJECT
+public slots:
+    void doRandom(int low, int high) // 相比重载run外部还可以提供参数进来更灵活
+    {
+        QRandomGenerator RG(QTime::currentTime().msec());
+        long long result = 0;
+        QTime s;
+        s.start();
+        for (long long i = 0; i < MAXCOUNT; ++i)
+        {
+            result += RG.bounded(low,high);
+        }
+        auto t = s.elapsed() / 1000.0;
+        emit ready(QString("耗时: %1 s  结果: %2\n").arg(t).arg(result));
+    }
+signals:
+    void ready(const QString&res);
+};
+
+class Controller:public QObject
+{
+    Q_OBJECT
+    QThread mThread;
+    QStatusBar * mBar;
+public:
+    Controller(QStatusBar*bar) :mBar(bar) {
+        Calculator *worker = new Calculator;// ① 新建一个工作对象(如果是属于类的下方不需要连接deleteLater)
+        worker->moveToThread(&mThread); // ② 工作对象移入子线程
+        connect(&mThread, &QThread::finished, worker, &QObject::deleteLater); // ③ 线程结束后删除workder对象
+
+        connect(this, &Controller::start, worker, &Calculator::doRandom); // ④ 按钮需要操作start信号,这个信号绑定了要执行的计算
+        connect(worker, &Calculator::ready, this, &Controller::handleResults); // ⑤ 工作对象的完成信号可以反馈给控制器也要绑定好
+        mThread.start();
+    }
+    ~Controller() {
+        mThread.quit();
+        mThread.wait();
+    }
+private slots:
+    void handleResults(const QString& res)
+    {
+        mBar->showMessage(res);
+    }
+signals:
+    void start(int low,int high); // 这个信号可以和按钮的信号绑定
+};
+
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+    MyThread mThread;
+    Controller * mController;
+private:
+    QPushButton * btn1;
+    QPushButton * btn2;
+    QPushButton * btn3;
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+private slots:
+//    void onbtn1();
+//    void onbtn2();
+//    void onbtn3();
+};
+#endif // MAINWINDOW_H
