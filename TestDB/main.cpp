713af1180be07b27d7edc1ae6a59b893d2063200
chenbei97 <chenbei_electric@163.com>
2023-07-04 17:31:44 +0800

    更新代理有关内容

--- TestDB/main.cpp
+++ TestDB/main.cpp
@@ -1,8 +1,26 @@
-#include <QCoreApplication>
+﻿#include <QCoreApplication>
+#include <QtSql/QSqlDatabase>
+#include <QSqlError>
+#include <QDebug>
+#define mysql
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
+    qDebug()<<QSqlDatabase ::drivers();
+#ifdef sqlite
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");//QMYSQL
+#else
+    QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
+    db.setHostName("127.0.0.1");  //连接本地主机
+    db.setPort(3306);
+    db.setUserName("root");
+    db.setPassword("199791");
+#endif
+    db.setDatabaseName("mdb"); // 会自动创建
+    if (!db.open())
+    {
+        qDebug()<<db.lastError().text();
+    }else {qDebug()<<"successful!";}
     return a.exec();
 }
