5dc4c5b4308b9e163df2f15e22ec73fb5571c727
chenbei97 <chenbei_electric@163.com>
2023-07-06 08:10:52 +0800

    u

--- TestIniSetting/main.cpp
+++ TestIniSetting/main.cpp
@@ -6,8 +6,8 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     QSettings * settings = new QSettings (":/config.ini", QSettings::IniFormat);
-    qDebug()<< settings->value("username")<<settings->value("password")
-            <<settings->value("dbdir").toString();
+    qDebug()<< settings->value("system/user")<<settings->value("system/password")
+            <<settings->value("system/dbdir").toString();
     //settings->deleteLater();
     return a.exec();
 }
