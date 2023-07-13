356ed6f3a8633163af75c194bccca0e382e6c662
chenbei97 <chenbei_electric@163.com>
2023-07-13 17:29:25 +0800

    update

---
+++ TestSplash/splash.cpp
@@ -0,0 +1,6 @@
+#include "splash.h"
+
+Splash::Splash()
+{
+
+}
---
+++ TestSplash/splash.h
@@ -0,0 +1,12 @@
+#ifndef SPLASH_H
+#define SPLASH_H
+
+
+class Splash : public QSplashScreen
+{
+    Q_OBJECT
+public:
+    Splash();
+};
+
+#endif // SPLASH_H
