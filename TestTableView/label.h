8a03902108ab14d125a4d862cd11f70572a16dc7
chenbei97 <chenbei_electric@163.com>
2023-07-20 17:29:51 +0800

    update table

---
+++ TestTableView/label.h
@@ -0,0 +1,36 @@
+﻿#ifndef LABEL_H
+#define LABEL_H
+
+#include <qlabel.h>
+#include <qgraphicseffect.h>
+#include <qpropertyanimation.h>
+#include <qfiledialog.h>
+#include <qmessagebox.h>
+#include "Constant.h"
+#include "Style.h"
+
+class Label : public QLabel
+{
+    Q_OBJECT
+private:
+    QGraphicsOpacityEffect * mEffect;
+    QPropertyAnimation * mAnimation;
+    bool mEffectSwitch;
+    bool mPressEvent;
+public:
+    explicit Label(QWidget*parent=nullptr);
+    explicit Label(const QString&text,QWidget*parent=nullptr);
+
+    ~Label();
+
+    void enableEffect();
+    void disableEffect();
+    void mousePressEvent(QMouseEvent *e) override;
+
+    void enablePressEvent();
+    void disablePressEvent();
+    void showEvent(QShowEvent*) override;
+
+};
+
+#endif // LABEL_H
